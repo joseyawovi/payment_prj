45e7b92f4255574e9a85b35e5a6d7c385b7509d2
joseyawovi Yawovi <joseyawovi@gmail.com>
2025-03-06 06:10:39 +0000

    waiting for static files

--- userauths/views.py
+++ userauths/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 def RegisterView(request):
-    form = UserRegisterForm()
-    context = {
-        'form':form
-    }
-    return render(request,"userauths/sign-up.html")
+    form = UserRegisterForm() 
+
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, your account was created successfully")
+
+            new_user = authenticate(username=form.cleaned_data["email"],  # ✅ Fixed dictionary access
+                                    password=form.cleaned_data["password1"])
+
+            if new_user is not None:
+                login(request, new_user)
+                return redirect("core:index")
+
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in.")
+        return redirect("core:index")
+        
+
+    context = {"form": form}
+    return render(request, "userauths/sign-up.html", context)
